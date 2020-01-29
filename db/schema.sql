572d85a04115f0938f6f870ed5e978d2916bfaea
jaredbucko <jaredbucko@icloud.com>
2020-01-29 11:07:11 -0800

    update file structure

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,12 @@
 DROP DATABASE IF EXISTS burgers_db;
 
--- Create the database burgers_db and specified it for use.
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
--- Create the table plans.
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burger_name varchar(255) NOT NULL,
   devoured BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
-
--- Insert a set of records.
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', 0);
---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', 0);
