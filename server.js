895c8da11e6bfba9de77454f3d6a4869d9080eab
jaredbucko <jaredbucko@icloud.com>
2020-02-03 19:37:18 -0800

    fixed issue with express

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
-var app = express();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // serve content for the app from the "public" directory
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 // parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // set handlebars
-var handlebars = require("express-handlebars");
+const handlebars = require("express-handlebars");
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+const routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
 // start server
 app.listen(PORT, function() {
   // log when server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on PORT: " + PORT);
 });
