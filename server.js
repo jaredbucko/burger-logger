3523f4d12acd017603c6d41b60b6131c687e0757
jaredbucko <jaredbucko@icloud.com>
2020-02-03 19:45:34 -0800

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // serve content for the app from the "public" directory
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 
 // parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
